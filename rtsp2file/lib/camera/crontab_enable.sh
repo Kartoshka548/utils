ac954241e6f91959877698abca0944be6672c47e
Vlad Nikiporoff <vladnikiporov@gmail.com>
2016-01-11 00:57:41 -0500

    preparing to push files recorded and stored inside the camera

---
+++ rtsp2file/lib/camera/crontab_enable.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env sh
+
+# change it also below (in ftpush.sh itself)
+SCRIPTS_DIR=/home/scripts # $1 for CLI argument
+EXECUTABLE=ftpush.sh 
+
+mkdir $SCRIPTS_DIR
+
+
+# crontab line to pull from
+#-------------------------------------------------
+echo "*/1 * * * * $SCRIPTS_DIR/$EXECUTABLE" > $SCRIPTS_DIR/crontab.bkp 
+
+# testing cron that it works with simple echo loop below
+touch $SCRIPTS_DIR/heys.txt
+
+
+# actual script to be executed with cron
+#-------------------------------------------------
+cat $SCRIPTS_DIR/$EXECUTABLE << EOF
+#!/usr/bin/env sh  
+DIR=/home/scripts
+echo 'hey_'+ $(date +\%Y-\%m-\%d_\%Hh\%Mm\%Ss) >> $DIR/heys.txt
+EOF
+
+# make it executable
+#-------------------------------------------------
+chmod +x $SCRIPTS_DIR/$EXECUTABLE
+
+
+
+# enable cron
+#-------------------------------------------------
+mkdir -p /var/spool/cron/crontabs
+
+# install cronjob from local backup file
+crontab $SCRIPTS_DIR/crontab.bkp
+
+# schedule it
+crond
+
