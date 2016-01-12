f07f2de45af6624c687cce0c32c883165c67c87e
Vlad Nikiporoff <vladnikiporov@gmail.com>
2016-01-12 01:09:12 -0500

    camera updates own public ip with duckdns at kartoshka.duckdns.org

--- rtsp2file/lib/camera/crontab_enable.sh
+++ rtsp2file/lib/camera/crontab_enable.sh
@@ -2,31 +2,43 @@
 
 # change it also below (in ftpush.sh itself)
 SCRIPTS_DIR=/home/scripts # $1 for CLI argument
-EXECUTABLE=ftpush.sh 
+
+FTP_PUSH=ftpush.sh 
+EXECUTABLE=update_public_dns.sh
+LOG=public_ip.log
+CRONTAB=crontab.bkp
 
 mkdir $SCRIPTS_DIR
+touch $SCRIPTS_DIR/$LOG
 
 
-# crontab line to pull from
-#-------------------------------------------------
-echo "*/1 * * * * $SCRIPTS_DIR/$EXECUTABLE" > $SCRIPTS_DIR/crontab.bkp 
-
-# testing cron that it works with simple echo loop below
-touch $SCRIPTS_DIR/public_ip
-
-
-# actual script to be executed with cron - discover self public IP (and post it somewhere later on) 
+# actual script to be executed with cron - discover self public IP 
 #-------------------------------------------------
 cat $SCRIPTS_DIR/$EXECUTABLE << EOF
-#!/usr/bin/env sh  
-DIR=/home/scripts
-echo $(date +\%Y-\%m-\%d_\%Hh\%Mm\%Ss)': '$(wget http://ipinfo.io/ip -qO -) > $DIR/public_ip
+#!/usr/bin/env sh
+
+LOG=$1
+PUBLIC_IP=$(wget http://ipinfo.io/ip -qO -)
+DUCK_DOMAIN=##########################
+DUCK_TOKEN=##########################
+
+echo '[' $(date +\%Y-\%m-\%d\ \%Hh\%Mm\%Ss) '] ip:' $PUBLIC_IP 'duckdns.org update:' $(wget -q http://duckdns.org/update/$DUCK_DOMAIN/$DUCK_TOKEN -O -) >> $LOG
 EOF
+
 
 # make it executable
 #-------------------------------------------------
 chmod +x $SCRIPTS_DIR/$EXECUTABLE
 
+
+# write local cron jobs file to pull from
+#-------------------------------------------------
+echo "# every minute" > $SCRIPTS_DIR/$CRONTAB
+echo "#*/1 * * * * $SCRIPTS_DIR/$FTP_PUSH $SCRIPTS_DIR/$LOG" >> $SCRIPTS_DIR/$CRONTAB
+echo "#* * * * * $SCRIPTS_DIR/$EXECUTABLE $SCRIPTS_DIR/$LOG" >> $SCRIPTS_DIR/$CRONTAB
+echo "" >> $SCRIPTS_DIR/$CRONTAB
+echo "# every half-hour" >> $SCRIPTS_DIR/$CRONTAB
+echo "0,30 * * * * $SCRIPTS_DIR/$EXECUTABLE $SCRIPTS_DIR/$LOG" >> $SCRIPTS_DIR/$CRONTAB
 
 
 # enable cron
@@ -34,7 +46,7 @@ chmod +x $SCRIPTS_DIR/$EXECUTABLE
 mkdir -p /var/spool/cron/crontabs
 
 # install cronjob from local backup file
-crontab $SCRIPTS_DIR/crontab.bkp
+crontab $SCRIPTS_DIR/$CRONTAB
 
 # schedule it
 crond
